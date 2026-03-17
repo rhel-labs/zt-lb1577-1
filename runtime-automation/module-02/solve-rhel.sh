84a52f65611371e5511486f47cdb08d511342d4c
Nate Lager <nlager@redhat.com>
2026-03-17 15:55:42 -0400

    module 2 about done, started 3

--- runtime-automation/module-02/solve-rhel.sh
+++ runtime-automation/module-02/solve-rhel.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+sudo grep '^DocumentRoot' /etc/httpd/conf/httpd.conf
+sudo ls /var/www/html
+sudo cat /var/www/html/index.html
+ps aux | grep httpd
+sudo ls -l /var/www/
+sudo ls -lZ /var/www
+
 echo "Solved module called module-02" >> /tmp/progress.log
--- runtime-automation/module-03/solve-rhel.sh
+++ runtime-automation/module-03/solve-rhel.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+restorecon -vFR /var/www/
+chown -R root:root /var/www/html
 echo "Solved module called module-03" >> /tmp/progress.log
